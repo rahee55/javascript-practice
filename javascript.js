1593e65d903d912e10927ee8d4e86b46151a42aa
tariq <rahee.programmer@gmail.com>
2024-07-23 14:19:12 +0500

    Javascript Practice Project

--- javascript.js
+++ javascript.js
@@ -451,48 +451,72 @@
 // LMS system
 
 
-let Data = prompt('enter (get) to display data')
+// let Data = prompt('enter (get) to display data')
 
-let students =[
-    {name :'faheem',fatherName : 'Nasrullah',age : 23,class : 'BSSE',roleNumber : 23,marks : 309},
-    {name :'zeeshan',fatherName : 'ramzan',age : 22,class : 'BSSE',roleNumber : 19,marks : 389},
-    {name :'adil',fatherName : 'amin',age : 21,class : 'BSSE',roleNumber : 10,marks : 299},
-    {name :'rehmat',fatherName : 'sadiq',age : 20,class : 'BSSE',roleNumber : 4,marks : 359},
-    {name :'tariq',fatherName : 'raheem',age : 22,class : 'BSSE',roleNumber : 1,marks : 390}
+// let students =[
+//     {name :'faheem',fatherName : 'Nasrullah',age : 23,class : 'BSSE',roleNumber : 23,marks : 309},
+//     {name :'zeeshan',fatherName : 'ramzan',age : 22,class : 'BSSE',roleNumber : 19,marks : 389},
+//     {name :'adil',fatherName : 'amin',age : 21,class : 'BSSE',roleNumber : 10,marks : 299},
+//     {name :'rehmat',fatherName : 'sadiq',age : 20,class : 'BSSE',roleNumber : 4,marks : 359},
+//     {name :'tariq',fatherName : 'raheem',age : 22,class : 'BSSE',roleNumber : 1,marks : 390}
+// ]
+
+// // display data 
+
+// if(Data == 'get'){
+//     console.table(students);
+// }
+
+// //delete student data
+// let Delete = prompt('enter student name to delete data')
+
+// for(let i =0; i<students.length; i++){
+//     if(students[i].name == Delete){
+//         students.splice(i,1)
+//     }
+// }
+// console.table(students);
+
+// // update in object value 
+
+// let update = prompt('enter name to update')
+// let change = prompt('enter property which want to change')
+// let value = +prompt('enter your remarks');
+// for(let i =0; i<students.length; i++){
+//     if(students[i].name == update){
+//         if(change == "age"  ){
+//             students[i].age = value;
+//         }
+//         else if(change == 'marks'){
+//             students[i].marks = value
+//         }
+//     }
+// }
+// console.table(students);
+
+let info = [
+    {userName: 'tariq-raheem',password: 'Tariq@9544'}
 ]
 
-// display data 
+for(i = 0; i < 2;i++){
 
-if(Data == 'get'){
-    console.table(students);
-}
+    let Name = prompt('enter your username')
+    let pass = prompt('enter your password')
 
-//delete student data
-let Delete = prompt('enter student name to delete data')
-
-for(let i =0; i<students.length; i++){
-    if(students[i].name == Delete){
-        students.splice(i,1)
+    if(info[0].userName == Name && info[0].password == pass){
+        alert('Login successfull') 
+        break;
     }
-}
-console.table(students);
+    else if(info[0].password !== pass){
+        alert('Wrong Password! you need to Forget your Password')
+    } 
+    confirm('are you sure you want to reset password');
 
-// update in object value 
+    let user = prompt('enter new password');
 
-let update = prompt('enter name to update')
-let change = prompt('enter property which want to change')
-let value = +prompt('enter your remarks');
-for(let i =0; i<students.length; i++){
-    if(students[i].name == update){
-        if(change == "age"  ){
-            students[i].age = value;
-        }
-        else if(change == 'marks'){
-            students[i].marks = value
-        }
-        
-    }
+    info[0].password = user;
 }
-console.table(students);
+
+
 
 
